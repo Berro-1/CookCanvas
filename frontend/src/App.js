bf18401b431cee9588f83de09d5b8e1547df55f8
Berro-1 <hussienberro1@gmail.com>
2024-07-22 18:27:51 +0300

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </>
+    )
+  );
+
   return (
     <div className="App">
-      
+      <RouterProvider router={router} />
     </div>
   );
 }
