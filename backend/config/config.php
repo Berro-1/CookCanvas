49f224fe1aff13c98266d49907a82075caba77fd
Berro-1 <hussienberro1@gmail.com>
2024-07-22 09:14:26 +0300

    added db configuration

---
+++ backend/config/config.php
@@ -0,0 +1,13 @@
+<?php
+$db = 'cookcanvas';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die('Connection error: ' . mysqli_connect_error());
+}
+
+echo "Connected successfully";
